e1ad282124376a7fd32364c23e1bc94be3cdc846
HenriFourie <61292359+henrifourieIO@users.noreply.github.com>
2021-05-21 12:10:56 +0200

    reupload contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -2,12 +2,12 @@ import Head from 'next/head'
 import Nav from '../components/Nav';
 import styles from '../styles/ContactPage.module.css';
 import ContactForm from "../components/ContactForm";
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { createGlobalStyle } from 'styled-components';
 
 export default function Contact() {
 
-    const [logo, setLogo] = useState("/image/logo.png")
+    const logo = "/image/logo.png";
 
     const GlobalStyle = createGlobalStyle`
     html {
