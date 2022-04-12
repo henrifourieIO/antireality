02493e1c56421573682534b29bc77f7aa42bf4a6
henri@fusebox.co.za <henri@fusebox.co.za>
2022-04-12 20:17:39 +0200

    removed backend pulls

--- pages/contact.js
+++ pages/contact.js
@@ -5,53 +5,7 @@ import ContactForm from "../components/ContactForm";
 import React, { useState } from 'react';
 import { createGlobalStyle } from 'styled-components';
 
-export async function getStaticProps() {
-    const headers = { 'Content-Type': 'application/json', 'Accept': 'application/json, text/plain, */*',
-    'User-Agent': '*', }
-    
-    const fetchAPI = await fetch("https://backend.antireality.co.za/ql", {
-        method: "POST",
-        headers,
-        body: JSON.stringify({
-            query: 
-            `
-                query MyQuery {
-                    page(id: "cG9zdDo2NQ==") {
-                    seo {
-                        metaDesc
-                        focuskw
-                        metaKeywords
-                        title
-                        twitterTitle
-                        twitterDescription
-                    }
-                    slug
-                    }
-                }
-            `
-        })
-    })
-
-    const json = await fetchAPI.json()
-    if (json.errors) {
-      console.error(json.errors)
-      throw new Error('Failed to fetch API')
-    }
-    return {
-        props: {
-            metaDesc: json.data.page.seo.metaDesc,
-            focuskw: json.data.page.seo.focuskw,
-            metaKeywords: json.data.page.seo.metaKeywords,
-            title: json.data.page.seo.title,
-            twitterTitle: json.data.page.seo.twitterTitle,
-            twitterDescription: json.data.page.seo.twitterDescription,
-            slug: json.data.page.slug,
-        },
-    }
-  }
-
-export default function Contact(data) {
-    console.log(data)
+export default function Contact() {
     const logo = "/image/logo.png";
 
     const GlobalStyle = createGlobalStyle`
@@ -64,13 +18,9 @@ export default function Contact(data) {
     return (
         <>
             <Head>
-                <title>{data.title}</title>
+                <title>Contact</title>
                 <link rel="icon" href="/image/favicon.ico" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="description" content={data.metaDesc} />
-                <meta name="keywords" content={data.metaKeywords} />
-                <meta property="og:title" content={data.twitterTitle} />
-                <meta property="og:description" content={data.twitterDescription} />
             </Head>
             <GlobalStyle />
             <Nav
